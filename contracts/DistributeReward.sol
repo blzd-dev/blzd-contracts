1f08fb510a923fd0390cc91eec51fe8531eec41a
blzd-dev <79627862+blzd-dev@users.noreply.github.com>
2021-03-10 12:45:34 +0700

    add constant

--- contracts/DistributeReward.sol
+++ contracts/DistributeReward.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 contract DistributeReward {
     using SafeMath for uint256;
     
-    address public marketing = 0x10346e91429E0676dCC13d67C0e3F6E9A028207F;
-    address public owner = 0xCe059E8af96a654d4afe630Fa325FBF70043Ab11;
-    address public mods = 0xb1155a0f8C7F34632dA1D516AF6591dE75c0eCFd;
-    address public dev = 0xbA828b976bFf1467E23Cb59bf2FFF6B81d8829C0;
+    address public constant marketing = 0x10346e91429E0676dCC13d67C0e3F6E9A028207F;
+    address public constant owner = 0xCe059E8af96a654d4afe630Fa325FBF70043Ab11;
+    address public constant mods = 0xb1155a0f8C7F34632dA1D516AF6591dE75c0eCFd;
+    address public constant dev = 0xbA828b976bFf1467E23Cb59bf2FFF6B81d8829C0;
     IBEP20 public blzd;
 
     constructor(address _blzd) public {
